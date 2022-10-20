6f20979b20a5fa5406b1b3ab88926ec3ac4f2c21
Navid <navidshad72@gmail.com>
2022-10-20 23:00:05 +0300

    add ELB dns for flutter apps

--- user_app/lib/settings/static_vars.dart
+++ user_app/lib/settings/static_vars.dart
@@ -1,4 +1,4 @@
 class Vars {
-  // static String host = 'https://haikusushi.se';
-  static String host = 'http://192.168.1.92:2005';
+  static String host = 'EC2Co-EcsEl-1OP6ZYHA6A2PA-1320295553.us-east-1.elb.amazonaws.com';
+  // static String host = 'http://192.168.1.92:2005';
 }
