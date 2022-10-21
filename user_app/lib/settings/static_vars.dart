d480a6b8f81a5fc0d0b56d8ceccd2c39e5cbd3e9
Navid <navidshad72@gmail.com>
2022-10-22 01:55:10 +0300

    changed server host name

--- user_app/lib/settings/static_vars.dart
+++ user_app/lib/settings/static_vars.dart
@@ -1,5 +1,5 @@
 class Vars {
   static String host =
-      'http://ec2co-ecsel-1op6zyha6a2pa-1320295553.us-east-1.elb.amazonaws.com';
+      'http://ec2co-ecsel-1gp3cytj0gkr7-2119626231.us-east-1.elb.amazonaws.com';
   // static String host = 'http://192.168.1.92:2005';
 }
